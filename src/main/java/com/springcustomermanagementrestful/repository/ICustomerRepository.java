6a4a297ae562b507e3c9ffc15137e2f1c672a007
LeVanChuyen-20 <20222411@eaut.edu.vn>
2024-12-26 09:12:54 +0700

    up

---
+++ src/main/java/com/springcustomermanagementrestful/repository/ICustomerRepository.java
@@ -0,0 +1,10 @@
+package com.springcustomermanagementrestful.repository;
+
+
+import com.springcustomermanagementrestful.model.Customer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+}
---
+++ src/main/java/com/springcustomermanagementrestful/service/ICustomerService.java
@@ -0,0 +1,6 @@
+package com.springcustomermanagementrestful.service;
+
+import com.springcustomermanagementrestful.model.Customer;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
